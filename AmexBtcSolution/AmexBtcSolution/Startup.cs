d91e993fb8ffcedd04bcf17050a964ed1a477118
jayantaMLM <sri.jayanta.chakrabarty@outlook.com>
2017-07-08 17:03:39 +0530

    First Commit

---
+++ AmexBtcSolution/AmexBtcSolution/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AmexBtcSolution.Startup))]
+namespace AmexBtcSolution
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
